703d82cb05ec86b8135a07d538f34b7c6ca25d01
tadao <tadao_kobayashi@diveintocode.jp>
2023-02-24 13:19:07 +0900

    エラー検証のためバリデーションを追加

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,4 +1,5 @@
 class Label < ApplicationRecord
   has_many :labelings, dependent: :destroy
   has_many :users, through: :labelings, source: :user
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :labelings, dependent: :destroy
   has_many :labels, through: :labelings, source: :label
+  validates :name, presence: true
+  validates :email, presence: true
 end
